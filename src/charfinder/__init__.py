f566f72428f075ddc925658c2673b41c00510170
berserkhmdvhb <hamed.vaheb@gmail.com>
2025-06-26 14:58:39 +0200

    Release 1.1.8 - docs: polish README

--- src/charfinder/__init__.py
+++ src/charfinder/__init__.py
@@ -8,7 +8,7 @@ from .core import find_chars, find_chars_raw
 from .core.name_cache import build_name_cache
 from .utils.normalizer import normalize
 
-__version__ = "1.1.7"
+__version__ = "1.1.8"
 
 __all__ = [
     "build_name_cache",
